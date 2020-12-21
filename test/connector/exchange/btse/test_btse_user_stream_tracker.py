da313d909bf09c8a1dfe294e4399d583b01962f2
thehapax <39811582+thehapax@users.noreply.github.com>
2020-12-20 16:45:00 -0800

    temporary update

--- test/connector/exchange/btse/test_btse_user_stream_tracker.py
+++ test/connector/exchange/btse/test_btse_user_stream_tracker.py
@@ -2,7 +2,7 @@
 
 import sys
 import asyncio
-import logging
+# import logging
 import unittest
 import conf
 
@@ -10,6 +10,11 @@ from os.path import join, realpath
 from hummingbot.connector.exchange.btse.btse_user_stream_tracker import BtseUserStreamTracker
 from hummingbot.connector.exchange.btse.btse_auth import BtseAuth
 from hummingbot.core.utils.async_utils import safe_ensure_future
+from hummingbot.connector.exchange.btse import btse_constants as Constants
+
+import requests
+import json
+
 
 sys.path.insert(0, realpath(join(__file__, "../../../")))
 
@@ -28,16 +33,32 @@ class BtseUserStreamTrackerUnitTest(unittest.TestCase):
         cls.user_stream_tracker_task: asyncio.Task = safe_ensure_future(cls.user_stream_tracker.start())
 
     def test_user_stream(self):
-        # Wait process some msgs.
-        print("inside test_user_stream.... waiting 120 seconds...")
-        self.ev_loop.run_until_complete(asyncio.sleep(60.0))
+        # Wait process some msgs. this should only contain content if there is a user trade
+        # otherwise no data will be sent.
+        print("inside test_user_stream.... waiting 60 seconds...")
+        self.ev_loop.run_until_complete(asyncio.sleep(10.0))
+
+        limit_order_form = {
+            "price": 7050,
+            "side": "BUY",
+            "size": 0.002,
+            "symbol": "BTC-USDT",
+            "time_in_force": "GTC",
+            "triggerPrice": 0,
+            "txType": "LIMIT",
+            "type": "LIMIT",
+            "clOrderID": "MYOWNORDERID2",
+        }
+        path = '/api/v3.2/order'
+        r = requests.post(
+            Constants.REST_URL + 'order',
+            json=limit_order_form,
+            headers=self.btse_auth.get_headers(path, json.dumps(limit_order_form))
+        )
+        print(r.text)
+
+        self.ev_loop.run_until_complete(asyncio.sleep(5.0))
         print(self.user_stream_tracker.user_stream)
 
-
-def main():
-    logging.basicConfig(level=logging.INFO)
-    unittest.main()
-
-
-if __name__ == "__main__":
-    main()
+        print("last recv time")
+        print(self.user_stream_tracker.last_recv_time)
