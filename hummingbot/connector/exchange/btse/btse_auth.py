260099ad605257288b23d061720bedd392963fda
thehapax <39811582+thehapax@users.noreply.github.com>
2020-12-20 17:02:25 -0800

    temporary update

--- hummingbot/connector/exchange/btse/btse_auth.py
+++ hummingbot/connector/exchange/btse/btse_auth.py
@@ -6,6 +6,8 @@ from typing import (
 import hashlib
 import hmac
 from hummingbot.connector.exchange.btse.btse_tracking_nonce import get_tracking_nonce
+import time
+import conf
 
 
 class BtseAuth:
@@ -42,19 +44,26 @@ class BtseAuth:
         }
         return auth_payload
 
-    def get_headers(self, method: str, path_url: str, data: str = "") -> Dict[str, any]:
+    def get_headers(self, path_url: str, data: str = "") -> Dict[str, any]:
         """
         Generates authentication headers required by btse
-        :param method: GET / POST / etc.
         :param path_url: e.g. "/accounts"
-        :param body: request payload
+        :param data: request payload
         :return: a dictionary of auth headers
         """
+        print("******* INSIDE GET HEADERS *******")
+        # print(f'\n SECRET KEY : {self.secret_key}, API-KEY : {self.api_key} ')
+        # api_secret: bytes = bytes(self.secret_key, 'latin-1')
+        # api_key: bytes = bytes(self.api_key, 'latin-1')
+
         nonce = get_tracking_nonce()
+        # nonce = str(int(time.time()*1000))
+        print(f'\nget_tracking_nonce: {nonce}')
+
         message = path_url + nonce + data
+        print(f'MESSAGE: {message}')
 
-        headers = {}
-
+        # bheaders = {}
         signature = hmac.new(
             bytes(self.secret_key, 'latin-1'),
             msg=bytes(message, 'latin-1'),
@@ -64,6 +73,36 @@ class BtseAuth:
         headers = {
             'btse-api': self.api_key,
             'btse-nonce': nonce,
-            'btse-sign': signature
+            'btse-sign': signature,
+            'Accept': 'application/json;charset=UTF-8',
+            'Content-Type': 'application/json',
         }
+        return headers
+
+    def make_headers(self, path: str, data: str = "") -> Dict[str, any]:
+        print("******* INSIDE MAKE HEADERS *******")
+        API_KEY = conf.btse_api_key
+        API_SECRET = conf.btse_secret_key
+        nonce = str(int(time.time() * 1000))
+        print("nonce:" + nonce)
+        message = path + nonce + data
+        print(f'MESSAGE - make_headers: {message}')
+        headers = {}
+        passph = API_SECRET
+        print(f'api-pass: {passph}')
+
+        signature = hmac.new(
+            bytes(API_SECRET, 'latin-1'),
+            msg=bytes(message, 'latin-1'),
+            digestmod=hashlib.sha384
+        ).hexdigest()
+
+        headers = {
+            'btse-api': API_KEY,
+            'btse-nonce': nonce,
+            'btse-sign': signature,
+            'Accept': 'application/json;charset=UTF-8',
+            'Content-Type': 'application/json',
+        }
+        print(headers)
         return headers
